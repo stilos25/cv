27f017b8d87b17185a38497bea5c3a0eec42255a
Spiros Baxevanakis <spiros.baxevanakis@gmail.com>
2019-03-04 12:56:19 +0200

    improve travisci config

---
+++ push.sh
@@ -0,0 +1,19 @@
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_pdf() {
+  git checkout master
+  git add cv.pdf
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/cv.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin
+}
+
+setup_git
+commit_pdf
+upload_files
